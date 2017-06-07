5a5bc9e69108136ca04d34da3e6fe4ff07c14dc0
Jose Barrios <jose@cognilab.com>
2017-06-07 02:26:16 -0700

    Added sub-category test

--- tests/test.js
+++ tests/test.js
@@ -3,16 +3,21 @@
 var assert = require('assert');
 var Person = require('../index.js');
 
-var model = {};
-model.givenName = 'jose';
-model.familyName = 'barrios';
-model.email = 'jose@barrios.io';
-let person = new Person(model);
+var personModel = {};
+personModel.givenName = 'jose';
+personModel.familyName = 'barrios';
+personModel.email = 'jose@barrios.io';
+personModel.affiliation = {};
+personModel.affiliation.name = 'BeVisible';
+let person = new Person(personModel);
+
+
 
 const key = {};
 key.fullName = 'Jose Barrios';
 key.givenName = 'Jose';
 key.familyName = 'Barrios';
+key.affiliationName = 'BeVisible';
 
 
 describe('person.fullName', function() {
@@ -30,5 +35,11 @@ describe('person.givenName', function() {
 describe('person.familyName', function() {
 	it('should capitalize family name', function() {
 		assert.equal(person.familyName, key.familyName);
+	});
+});
+
+describe('person.affiliation.name', function() {
+	it('should return the name of the organization the person is affiliated to', function() {
+		assert.equal(person.affiliation.name, key.affiliationName);
 	});
 });
