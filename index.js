7d14a109430e54f4d54f6a9da96ca0b6807cff42
Jose Barrios <jose@cognilab.com>
2017-06-19 15:43:00 -0700

    Modified getters and setters so empty values don't return empty strings, but instead undefined

--- index.js
+++ index.js
@@ -35,80 +35,87 @@ class Person extends Thing {
 
   get address(){ return this.model.address || null; }
   set address(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.address = value;
+    if(!value || value === EMPTY_STRING) { this.model.address = undefined; }
+    else { this.model.address = value; }
   }
+
   get affiliation(){ return this.model.affiliation || null; }
   set affiliation(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.affiliation = value;
+    if(!value || value === EMPTY_STRING){ this.model.affiliation = undefined }
+    else { this.model.affiliation = value; }
   }
 
   get email(){ return this.model.email || null; }
   set email(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.email = value.toLowerCase();
+    if(!value || value === EMPTY_STRING) { this.model.email = undefined; }
+    else { this.model.email = value.toLowerCase(); }
   }
 
   get familyName(){ return this.model.familyName || null; }
   set familyName(value){
-    if(!value || value === EMPTY_STRING) return;
-    let lowerCase = value.toLowerCase();
-    this.model.familyName = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+    if(!value || value === EMPTY_STRING) { this.model.familyName = undefined; }
+    else{
+      let lowerCase = value.toLowerCase();
+      this.model.familyName = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+    }
   }
 
   get follows(){ return this.model.follows || null; }
   set follows(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.follows = value;
+    if(!value || value === EMPTY_STRING) { this.model.follows = undefined; }
+    else { this.model.follows = value; }
   }
 
 
   get gender(){ return this.model.gender || null; }
   set gender(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.gender = value;
+    if(!value || value === EMPTY_STRING) { this.model.gender = undefined; }
+    else { this.model.gender = value; }
   }
 
   get givenName(){ return this.model.givenName || null; }
   set givenName(value){
-    if(!value || value === EMPTY_STRING) return;
-    let lowerCase = value.toLowerCase();
-    this.model.givenName = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+    if(!value || value === EMPTY_STRING) { this.model.givenName = undefined; }
+    else {
+      let lowerCase = value.toLowerCase();
+      this.model.givenName = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+    }
   }
 
   get jobTitle(){ return this.model.jobTitle || null; }
   set jobTitle(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.jobTitle = value;
+    if(!value || value === EMPTY_STRING) { this.model.jobTitle = undefined; }
+    else { this.model.jobTitle = value; }
   }
 
   get taxID(){ return this.model.taxID || null; }
   set taxID(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.taxID = value;
+    if(!value || value === EMPTY_STRING) { this.model.taxID = undefined; }
+    else { this.model.taxID = value; }
   }
 
   get telephone(){ return this.model.telephone || null; }
   set telephone(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.telephone = value;
+    if(!value || value === EMPTY_STRING) { this.model.telephone = undefined; }
+    else { this.model.telephone = value; }
   }
+
   get vatID(){ return this.model.vatID || null; }
   set vatID(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.vatID = value;
+    if(!value || value === EMPTY_STRING) { this.model.vatID = undefined; }
+    else { this.model.vatID = value; }
   }
+
   get worksFor(){ return this.model.worksFor || null; }
   set worksFor(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.worksFor = value;
+    if(!value || value === EMPTY_STRING) { this.model.worksFor = undefined; }
+    else { this.model.worksFor = value; }
   }
 
   get image(){ return this.model.image || null; }
   set image(value){
-    if(!value || value === EMPTY_STRING) return;
-    this.model.image = value;
+    if(!value || value === EMPTY_STRING) { this.model.image = undefined; }
+    else { this.model.image = value; }
   }
 
 
